da85cd5eb989f075d67cc57104241d976f8213b7
mustafatok2016 <mustafa.tk33@gmail.com>
2020-08-25 22:56:06 +0000

    some configurations

--- igw.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_internet_gateway" "gw" {
-  vpc_id = "${aws_vpc.team2_vpc.id}"
-  tags   = "${var.tags}"
-}
--- vpc.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_vpc" "team2_vpc" {
-  cidr_block = "${var.cidr_block}"
-  tags       = "${var.tags}"
-}
