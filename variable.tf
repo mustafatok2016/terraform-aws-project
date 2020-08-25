42f33aac66e96e3795a6168c684e5f2ed1cd09b0
mustafatok2016 <mustafa.tk33@gmail.com>
2020-08-25 23:43:08 +0000

    some changes

--- variable.tf
+++ variable.tf
@@ -1,14 +1,3 @@
-variable "cidr_block" {}
-variable "region" {}
-
-variable "public_cidr1" {}
-variable "public_cidr2" {}
-variable "public_cidr3" {}
-
-variable "private_cidr1" {}
-variable "private_cidr2" {}
-variable "private_cidr3" {}
-
 variable "key_name" {}
 variable "image_owner" {}
 variable "desired_capacity" {}
